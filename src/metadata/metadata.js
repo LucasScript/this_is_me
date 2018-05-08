bcc28eca999c83bf26087b355b6498dd34e55b50
Lucas <lucasoli4@gmail.com>
2018-05-08 20:44:44 +0200

    first commit

---
+++ src/metadata/metadata.js
@@ -0,0 +1,55 @@
+module.exports = {
+  updated: new Date(),
+  title: 'Gysbert Lucas Olivier',
+  description: 'Software craftsman',
+  projectUrl: 'http://LucasScript.github.io/my-cv-repo',
+  details: {
+    details: ['Sandton, 2191, South Africa',
+      '+27718951225',
+      'lucasoli4@gmail.com',
+    ],
+    'Date/Place of Birth': ['1991-01-30 / South Africa'],
+    'Nationality': ['South African']
+  },
+  social: [{
+    github: 'https://github.com/LucasScript'
+  },
+  {
+    linkedin: 'https://www.linkedin.com/in/LucasOli4',
+  }],
+  summary: {
+    'Professional Summary': 'This is how awesome I am.',
+  },
+  experience: {
+    'Employment history': [
+      { place: 'Investec', position: 'Software Developer', time: 'January 2014 - Present', description: 'First 2 + years worked on Java/Xml systems 2+ years moved to Javascript/Angular/node.js systems' },
+      { place: 'FOXit(Pty) LTD', position: 'Intern Developer', time: 'November 2012 - December 2012', description: 'Windows 8 application development' },
+      { place: 'Scadco', position: 'Project Assistant', time: 'July 2011 - July 2011', description: 'Assisting in Setting up computers and networks in schools' },
+    ],
+    'Education': [
+      { place: 'North-West University', type: 'Honors Bachelor of Science(BSc)', degree: 'Computer Science and Information Systems', time: '2013', description: 'I need to fill the area with some wonderful words' },
+      { place: 'North-West University', type: 'Bachelor of Science(BSc)', degree: 'Information Technology', time: '2010 - 2012', description: 'List of all the amazing stuff i wish i did' },
+      { place: 'High School Ben Vorster', type: 'National senior certificate', degree: '', time: '2005 - 2009', description: 'Who could remember all the fun we had in high school' }
+    ],
+  },
+  languages: [
+    { name: 'English', rating: '4' },
+    { name: 'Afrikaans', rating: '5' },
+  ],
+  skills: ['Team work', 'Communication'],
+  tech: [
+    { name: 'Java', rating: '4' },
+    { name: 'XML', rating: '3' },
+    { name: 'SQL', rating: '4' },
+    { name: 'HTML', rating: '5' },
+    { name: 'CSS', rating: '5' },
+    { name: 'SCSS', rating: '5' },
+    { name: 'JavaScript', rating: '5' },
+    { name: 'TypeScript', rating: '5' },
+    { name: 'Node.js', rating: '4' },
+    { name: 'Angular.js', rating: '4' },
+    { name: 'Bootstrap', rating: '5' }
+  ],
+  hobbies: ['Cycling', 'Photography', 'Arduino Projects'],
+  References: 'Available on request'
+};
---
+++ src/utils/pdf.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const pdf = require('html-pdf');
+const path = require('path');
+
+module.exports = function buildPdf(input, output) {
+  let html = fs.readFileSync(input, 'utf8');
+  let bootstrap = fs.readFileSync(path.join(__dirname, '../vendors/bootstrap/css/bootstrap.css'), 'utf8');
+  let fontawesome = fs.readFileSync(path.join(__dirname, '../vendors/fontawesome/fontawesome-all.css'), 'utf8');
+  let index = fs.readFileSync(path.join(__dirname, '../assets/index.css'), 'utf8');
+  const outputDir = __dirname + '/../../dest';
+
+  html = html.replace('@bootstrap@', bootstrap);
+  html = html.replace('@fontawesome@', fontawesome);
+  html = html.replace('@index@', index);
+
+  fs.writeFileSync(outputDir + '/pdf.html', html);
+
+  const options = {
+    format: 'A4',
+    orientation: 'portrait',
+    border: '2.54cm',
+  };
+
+  return new Promise((resolve, reject) => {
+    pdf.create(html, options).toFile(output, function (err) {
+      if (err) {
+        console.log(err);
+        reject(err);
+      }
+      resolve();
+    });
+  })
+}

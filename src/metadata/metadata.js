60ff0a292c165d3de62e2c13865d338e06020275
Lucas <lucasoli4@gmail.com>
2018-05-13 20:30:56 +0200

    did some styling

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -17,38 +17,36 @@ module.exports = {
   {
     linkedin: 'https://www.linkedin.com/in/LucasOli4',
   }],
-  summary: {
-    'Professional Summary': 'This is how awesome I am.',
+  profile: {
+    'Profile': 'This is how awesome I am. this should be a very long, but short paragraph of me, describing all the cool things i could do if i ever did put my head to it.',
   },
-  experience: {
-    'Employment history': [
-      { place: 'Investec', position: 'Software Developer', time: 'January 2014 - Present', description: 'First 2 + years worked on Java/Xml systems 2+ years moved to Javascript/Angular/node.js systems' },
-      { place: 'FOXit(Pty) LTD', position: 'Intern Developer', time: 'November 2012 - December 2012', description: 'Windows 8 application development' },
-      { place: 'Scadco', position: 'Project Assistant', time: 'July 2011 - July 2011', description: 'Assisting in Setting up computers and networks in schools' },
-    ],
-    'Education': [
-      { place: 'North-West University', type: 'Honors Bachelor of Science(BSc)', degree: 'Computer Science and Information Systems', time: '2013', description: 'I need to fill the area with some wonderful words' },
-      { place: 'North-West University', type: 'Bachelor of Science(BSc)', degree: 'Information Technology', time: '2010 - 2012', description: 'List of all the amazing stuff i wish i did' },
-      { place: 'High School Ben Vorster', type: 'National senior certificate', degree: '', time: '2005 - 2009', description: 'Who could remember all the fun we had in high school' }
-    ],
-  },
+  employment: [
+    { place: 'Investec', position: 'Software Developer', time: 'January 2014 - Present', description: 'First 2 + years worked on Java/Xml systems 2+ years moved to Javascript/Angular/node.js systems' },
+    { place: 'FOXit(Pty) LTD', position: 'Intern Developer', time: 'November 2012 - December 2012', description: 'Windows 8 application development' },
+    { place: 'Scadco', position: 'Project Assistant', time: 'July 2011 - July 2011', description: 'Assisting in Setting up computers and networks in schools' },
+  ],
+  education: [
+    { place: 'North-West University', type: 'Honors Bachelor of Science(BSc)', degree: 'Computer Science and Information Systems', time: '2013', description: 'I need to fill the area with some wonderful words' },
+    { place: 'North-West University', type: 'Bachelor of Science(BSc)', degree: 'Information Technology', time: '2010 - 2012', description: 'List of all the amazing stuff i wish i did' },
+    { place: 'High School Ben Vorster', type: 'National senior certificate', degree: '', time: '2005 - 2009', description: 'Who could remember all the fun we had in high school' }
+  ],
   languages: [
-    { name: 'English', rating: '4' },
-    { name: 'Afrikaans', rating: '5' },
+    { name: 'English', rating: '75' },
+    { name: 'Afrikaans', rating: '85' },
   ],
   skills: ['Team work', 'Communication'],
   tech: [
-    { name: 'Java', rating: '4' },
-    { name: 'XML', rating: '3' },
-    { name: 'SQL', rating: '4' },
-    { name: 'HTML', rating: '5' },
-    { name: 'CSS', rating: '5' },
-    { name: 'SCSS', rating: '5' },
-    { name: 'JavaScript', rating: '5' },
-    { name: 'TypeScript', rating: '5' },
-    { name: 'Node.js', rating: '4' },
-    { name: 'Angular.js', rating: '4' },
-    { name: 'Bootstrap', rating: '5' }
+    { name: 'Java', rating: '65' },
+    { name: 'XML', rating: '55' },
+    { name: 'SQL', rating: '65' },
+    { name: 'HTML', rating: '85' },
+    { name: 'CSS', rating: '85' },
+    { name: 'SCSS', rating: '80' },
+    { name: 'JavaScript', rating: '85' },
+    { name: 'TypeScript', rating: '80' },
+    { name: 'Node.js', rating: '85' },
+    { name: 'Angular.js', rating: '80' },
+    { name: 'Bootstrap', rating: '80' }
   ],
   hobbies: ['Cycling', 'Photography', 'Arduino Projects'],
   References: 'Available on request'
